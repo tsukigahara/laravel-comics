dc36a460ca311dcd78083a405430ec22d410fbd1
tsukigahara <satsuki.tksm@gmail.com>
2023-01-31 11:03:07 +0900

    added comics content

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,4 +1,39 @@
 @extends('layout.main')
+@section('head')
+<title>Comics</title>    
+@endsection
 @section('content')
-    <h2>hello world</h2>
+    <section class="comics">
+        <!-- jumbo div -->
+        <div class="jumbo" style="background-image: url('{{ Vite::asset('resources/img/jumbotron.jpg')}}')">
+            <!-- title -->
+            <div class="section_title">
+                CURRENT SERIES
+            </div>
+        </div>
+        <!-- contents -->
+        <div class="container p-5">
+            <!-- bootrap grid system -->
+            <div class="row row-cols-lg-6 row-cols-md-4 row-cols-sm-3 row-cols-2 g-3">
+                <!-- vfor using props -->
+                <div class="col">
+                    <a href="">
+                        <!-- card -->
+                        <div class="card bg-transparent">
+                            <!-- card image -->
+                            <img src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" class="card-img-top rounded-0" alt="">
+                            <!-- card boby -->
+                            <div class="card-body px-0">
+                                <!-- card title -->
+                                <h5 class="card-title my_card-title">ACTION COMICS</h5>
+                                <!-- <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                                    additional
+                                    content. This content is a little bit longer.</p> -->
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ Vite::asset('resources/img/favicon.ico') }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @yield('head')
-        @vite('resources/js/app.js')
-
+    @vite('resources/js/app.js')
 </head>
 <body>
     @include('components.header')
